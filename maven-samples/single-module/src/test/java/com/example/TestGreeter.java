099e9f19210ebd3171f524f83c79aea7d5e24b51
ademvishal06 <ademvishal06@gmail.com>
2025-01-12 11:51:08 +0530

    Added New Test Case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -35,6 +35,12 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
+  
+  @Test
+  public void intentionally_Failed() {
+    assertTrue(true);
+  }
+
     @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
